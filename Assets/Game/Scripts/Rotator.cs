04fa3fdce8891dfa79bcf4eb2800a25ed43281c2
Adi Zeligson <azeligson@fortinet.com>
2022-02-24 12:10:36 +0200

    added turning circle

--- Assets/Game/Scripts/Rotator.cs
+++ Assets/Game/Scripts/Rotator.cs
@@ -2,10 +2,12 @@
 
 public class Rotator : MonoBehaviour {
 
-	public float speed = 100f;
+	public float speed = 20f;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0f, 0f, speed * Time.deltaTime);
-	}
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        //transform.position -= (new Vector3(speed, speed, 0) * Time.deltaTime);
+        //transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
 }
